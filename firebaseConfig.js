67d50ea72be2474a187bf0dabd6de62bf0813a22
speedautosystems-waleed <139775342+speedautosystems-waleed@users.noreply.github.com>
2024-03-29 21:53:46 +0500

    CDL-DeleteAccount-Added

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+// firebaseConfig.js
+const firebaseConfig = {
+    apiKey: "AIzaSyCsXJcAcaCLg3ZiKE_ZtT_KvuyRQDJSFCk",
+    authDomain: "comericial-driving-license.firebaseapp.com",
+    projectId: "comericial-driving-license",
+    storageBucket: "comericial-driving-license.appspot.com",
+    messagingSenderId: "168647323282",
+    appId: "1:168647323282:web:28b8d8b2bcf187a228c04b",
+    measurementId: "G-CCRVQXBLWK"
+};
+
+export default firebaseConfig;
